bf236deb600c09104b886998aca804f5fa34f027
Spencer <spencermcmurray@yahoo.ca>
2018-12-30 16:42:27 -0500

    Deleting old files

--- Main.py
+++
@@ -1,8 +0,0 @@
-from CropSheet import find_squares
-from GetDigits import get_all
-
-
-def run(filepath, num_rounds):
-    imgs = find_squares(filepath, num_rounds)
-    marks = get_all(imgs)
-    return marks
