74c005359f2b425be6f63dc145980bd22bef418a
Spencer <spencermcmurray@yahoo.ca>
2019-01-16 22:57:00 -0500

    Changed to a remote database host to allow for coding on the go!

--- database.py
+++ database.py
@@ -3,11 +3,11 @@ import pymysql
 
 class Database:
     def __init__(self):
-        host = "127.0.0.1"
+        host = "den1.mysql5.gear.host"
         port = 3306
-        user = "root"
-        password = ""
-        db = "mydb"
+        user = "autofeistesting"
+        password = "password1@"
+        db = "autofeistesting"
 
         self.con = pymysql.connect(host=host, port=port, user=user, password=password, db=db,
                                    cursorclass=pymysql.cursors.DictCursor, autocommit=True)
