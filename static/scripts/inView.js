df2c38da84776f6746d1d4b326fda1775831e11a
Spencer <spencermcmurray@yahoo.ca>
2019-01-26 18:16:21 -0500

    Added animate.css for text animations

---
+++ static/scripts/inView.js
@@ -0,0 +1,13 @@
+function isAnyPartOfElementInViewport(el) {
+
+    var rect = el.getBoundingClientRect();
+    // DOMRect { x: 8, y: 8, width: 100, height: 100, top: 8, right: 108, bottom: 108, left: 8 }
+    var windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+    var windowWidth = (window.innerWidth || document.documentElement.clientWidth);
+
+    // http://stackoverflow.com/questions/325933/determine-whether-two-date-ranges-overlap
+    var vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0);
+    var horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0);
+
+    return (vertInView && horInView);
+}
