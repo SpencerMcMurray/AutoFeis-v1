6c0771a48f228089db26fa9cae98fc57d06b44e3
Spencer <spencermcmurray@yahoo.ca>
2019-03-10 11:49:16 -0400

    Blocked access to database for future, the current db is just for testing

--- database.py
+++ database.py
@@ -3,11 +3,11 @@ import pymysql
 
 class Database:
     def __init__(self):
-        host = "den1.mysql5.gear.host"
+        host = "hostname"
         port = 3306
-        user = "autofeistesting"
-        password = "password1@"
-        db = "autofeistesting"
+        user = "user"
+        password = "password"
+        db = "db"
 
         self.con = pymysql.connect(host=host, port=port, user=user, password=password, db=db,
                                    cursorclass=pymysql.cursors.DictCursor, autocommit=True)
